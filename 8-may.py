2db87a9208b49679354030d736a5e53b6155ff77
DEVENDRA-5470 <ysdevn@gmail.com>
2026-05-08 10:25:22 +0530

    8-may

---
+++ 8-may.py
@@ -0,0 +1,47 @@
+# Loops in python 
+# 1.for loop : ranged based 
+# 2.while loop : condition based
+
+# 1.for loop 
+# range always works with integer number.
+# range (start:0,stop-1,step:1)
+
+# s=0
+# for i in range(1,5):
+#     s=s+1
+# print(s)
+
+
+# for r in range(10,-3,-1):
+#     print(r)
+
+
+# for i in range(1,15):
+#     if i>=6:
+#         break
+#     print(i)
+
+# for i in range(1,15):
+#     if i==6:
+#         continue
+#     print(i)
+
+# 1.Wap to takes start_point and end_point from user input 
+# and print all number divisble by 2 and 3.
+
+# 2.Wap to take a number from user input and print formated table.
+# format : 
+# 3x1=3
+# 3x2=6
+...
+# 3x10=30
+
+# 3.Wap to take a number from user input and print reversed formated table.
+# format : 
+# 3x10=30
+# 3x2=6
+# 3x1=3
+
+
+     
+    
