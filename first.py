f4eea9c4687a8c6655e9e36c9a63b81c2c4851e6
DEVENDRA-5470 <ysdevn@gmail.com>
2026-05-15 19:05:52 +0530

    Revert "arvind-update-code"
    This reverts commit 4fd7306b341dfde5b9edf570b8e5d024a22be2e5.

---
+++ first.py
@@ -0,0 +1,39 @@
+# num1=2
+# num2=15
+# # sum1=num1+num2
+# # multi=num1*num2
+# # divide=num2%num1
+# power=num2**num1
+
+# # print(num1,"+",num2,"=",sum1)
+# print(f"{num1} and {num2}={power}")
+
+# # print(num1,"x",num2,"=",multi)
+
+
+
+
+# I/O : operation
+
+# by default input() returns string type value.
+
+# num1=int(input("Enter your number1 :"))
+# num2=int(input("Enter your number2: "))
+# res=num1+num2
+# print(res)
+
+
+# Type casting and Type conversion.
+
+# n=10
+# m=10
+# print(n+m)
+
+num=100
+name="Dev"
+# print(num+" "+name)
+
+print(type(name))
+print(type(num))
+
+
--- work.py
+++
@@ -1,1 +0,0 @@
-print("this is work by arvid")
