ecad8945bb36855aff091d24cadbd0cfcb0d2880
DEVENDRA-5470 <ysdevn@gmail.com>
2026-05-18 09:58:47 +0530

    Revert "Deleted"
    This reverts commit 4d1c61923035a6c9a9010fae159934bbe56f6d63.

---
+++ 14-may.py
@@ -0,0 +1,28 @@
+# while loop :
+    # initializer
+    # condition
+    # increament/decreament
+
+# i=1 # initializer
+# while i<=10: # condition
+#     print(i)
+#     i+=1 # increament/decreament
+
+# i=10
+# while i>=1:
+#     print(i)
+#     i-=1
+
+# wap to print only even number from 10 to 20.
+
+# start=10
+# end=20
+# while start <= end:
+#     if start%2==0:
+#         print(start)
+#     start+=3
+
+# Wap to print the total of even number from 1 to 15.
+# Wap to check the give string by user is "palindrome" or "not palindrome".
+# Wap to reverse the digit : 1234 output : 4321
+
---
+++ 7-may.py
@@ -0,0 +1,48 @@
+# name="Dev"
+# if name:
+#     print(f"Yes name {name} is provided")
+#     address="noida"
+#     if address:
+#         print(f"Yes address is : {address} provided" )
+#     else:
+#         print(f"Address is not provided")
+
+
+# else:
+#     print("Name is not Provided")
+
+
+
+# num1=20
+# if num1%2==0:
+#     print("EVEN")
+#     mob=input("Enter Your number : ")
+#     if len(mob) == 10:
+#         print("Valid number")
+
+#     else:
+#         print("Invalid number")
+# else:
+#     print("ODD")
+
+
+# student_name=input("Enter Your Name : ")
+# pre=int(input("Enter Your pre marks : "))
+# if pre >= 400:
+#     print("Your are pass in pre and eligble for mains")
+
+#     mains=int(input("Enter Your Mains Marks :"))
+#     if mains >= 600:
+#         print("Your are pass in Mains and eligble for Interview")
+
+#         inter=int(input("Enter your interview marks : "))
+#         if inter>=700:
+#             print(f"You are selected as IAS Mr.{student_name}")
+#         else:
+#             print(f"You are fail in Interview Mr.{student_name}")
+            
+#     else:
+#         print("Fail in mains ")
+
+# else:
+#     print("Better luck next time ,pre failed ❌")
