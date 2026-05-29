c310a14e8d23726dc7bfe03204da0f9940e5460b
DEVENDRA-5470 <ysdevn@gmail.com>
2026-05-29 10:24:29 +0530

    update-list

--- 26-may.py
+++ 26-may.py
@@ -39,10 +39,15 @@
 
 
 # Slicing.
-# marks=[10,20,30,40,50,60,70,80]
+# marks=[10,20,30,40,50,60,70,80,90,100]
 # [start-0:stop-1:step-1]
-# sub_list=marks[6:0:-1]
+# sub_list=marks[3:7]
+# sub_list=marks[2:9:2]
+# sub_list=marks[:3:-1]
 # print(sub_list)
+
+
+
 
 # 6.Traversing
 # marks=[10,11,20,31,30,33,40,55,50,60,70,80]
@@ -66,3 +71,25 @@
 # for i in marks:
 #     total=total+i
 # print(total)
+
+
+# Wap to swap the first value of list with last value of list.
+# my_list=[10,20,30,40]
+
+# first_elm=my_list[0]
+# last_elm=my_list[-1]
+
+# my_list[-1]=first_elm
+# my_list[0]=last_elm
+
+# print(my_list)
+
+
+# 1.Wap to find the sum of the all elments in the list : [10,20,30,40].
+# 2.Wap to find the sum of only even elments in the list : [10,3,4,6,22,31,33,55,40].
+# 3.Wap to find the sum of only odd elments in the list : [10,3,4,6,22,31,33,55,40].
+# 4.Wap to find the count of how many int value and how many str in the list 
+# : [70,"aman",50,10,20,"rohan","iq-india"].
+
+
+
